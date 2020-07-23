ecbf0592e2bebebb8a43cd261177822ddf1b448e
Shubham Mane <Shubhammane434@gmail.com>
2020-07-23 15:24:33 +0530

    assignment-3

--- module-3/app.js
+++ module-3/app.js
@@ -1,63 +1,63 @@
 (function () {
   'use strict';
-  
+
   angular.module('NarrowItDownApp', [])
-  .controller('NarrowItDownController', NarrowItDownController)
-  .service('MenuSearchService', MenuSearchService)
-  .directive('foundItems', FoundItemsDirective);
-  
+    .controller('NarrowItDownController', NarrowItDownController)
+    .service('MenuSearchService', MenuSearchService)
+    .directive('foundItems', FoundItemsDirective);
+
   NarrowItDownController.inject = ['MenuSearchService'];
   function NarrowItDownController(MenuSearchService) {
     var narrowItDown = this;
-  
+
     narrowItDown.found = [];
     narrowItDown.searchError = false;
-  
+
     narrowItDown.search = function () {
-        if (narrowItDown.searchTerm != null && narrowItDown.searchTerm != '') {
-            narrowItDown.searchError = false; // it's a valid search, so do not display error message
-            
-            MenuSearchService.getMatchedMenuItems(narrowItDown.searchTerm)
-                             .then(function (foundItems) {
-                narrowItDown.found = foundItems;
-  
-                if (narrowItDown.found.length == 0)
-                  narrowItDown.searchError = true;
-            });
-        } else { // invalid search
-          narrowItDown.found.length = 0; // clear array
-          narrowItDown.searchError = true;
-        }
+      if (narrowItDown.searchTerm != null && narrowItDown.searchTerm != '') {
+        narrowItDown.searchError = false; // it's a valid search, so do not display error message
+
+        MenuSearchService.getMatchedMenuItems(narrowItDown.searchTerm)
+          .then(function (foundItems) {
+            narrowItDown.found = foundItems;
+
+            if (narrowItDown.found.length == 0)
+              narrowItDown.searchError = true;
+          });
+      } else { // invalid search
+        narrowItDown.found.length = 0; // clear array
+        narrowItDown.searchError = true;
+      }
     };
-  
+
     narrowItDown.removeItem = function (itemIndex) {
       narrowItDown.found.splice(itemIndex, 1);
     };
   }
-  
+
   MenuSearchService.inject = ['$http'];
   function MenuSearchService($http) {
     var menuSearchService = this;
-  
+
     menuSearchService.getMatchedMenuItems = function (searchTerm) {
-      return $http({url: 'https://davids-restaurant.herokuapp.com/menu_items.json'}).then(function (result) {
-          
-          var foundItems = result.data.menu_items;
-  
-          for (var i = 0; i < foundItems.length; i++) {
-           
-            if (foundItems[i].description.toLowerCase().indexOf(searchTerm.toLowerCase()) === -1) {
-              foundItems.splice(i, 1); 
-              i--; 
-            }
+      return $http({ url: 'https://davids-restaurant.herokuapp.com/menu_items.json' }).then(function (result) {
+
+        var foundItems = result.data.menu_items;
+
+        for (var i = 0; i < foundItems.length; i++) {
+
+          if (foundItems[i].description.toLowerCase().indexOf(searchTerm.toLowerCase()) === -1) {
+            foundItems.splice(i, 1);
+            i--;
           }
-  
-          // return processed items
-          return foundItems;
+        }
+
+        // return processed items
+        return foundItems;
       });
     };
   }
-  
+
   function FoundItemsDirective() {
     var ddo = {
       templateUrl: 'foundItems.html',
@@ -66,8 +66,7 @@
         onRemove: '&'
       }
     };
-  
+
     return ddo;
   }
-  })();
-  
+})();
